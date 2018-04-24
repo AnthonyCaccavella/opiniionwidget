c754cb6a6fb9e497fd26aaad236db92ea679f43e
Anthony Caccavella <anthony@MacBook-Air-2.local>
2018-04-24 15:37:06 -0600

    api webhook call is fixed

--- src/server.js
+++ src/server.js
@@ -4,14 +4,17 @@ const cors = require('cors');
 
 let app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 
 
-app.post('/api/integrations', (req, res) => {        
-    res.send({
-        "testing": true
-    });                
+app.post('/api/integrations', (req, res) => {
+    // res.send("test " + req.body)
+    let uniqueid = req.body.unique_id;
+    let credential = req.body.credential;
+    let event = req.body.event;
+    res.send("done " + uniqueid + ' ' + credential + ' ' + event);                
 })
 
 
