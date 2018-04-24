19d8070c2cf86f13a0113e36d73320c659a26e50
Anthony Caccavella <anthony@MacBook-Air-2.local>
2018-04-24 15:47:29 -0600

    adjusting individual pieces of webhook call to make sure it works on live

--- src/server.js
+++ src/server.js
@@ -14,7 +14,11 @@ app.post('/api/integrations', (req, res) => {
     let uniqueid = req.body.unique_id;
     let credential = req.body.credential;
     let event = req.body.event;
-    res.send("done " + uniqueid + ' ' + credential + ' ' + event);                
+    res.send("done " + uniqueid + ' ' + credential + ' ' + event)
+//     .then(axios.get('https://api.smartwaiver.com/v4/waivers/'+uniqueid+'?pdf=false&sw-api-key=056eecb3218c7bbdf23c0335b81c47bc').then((response) => {
+//         return response.data;
+//     })
+// )
 })
 
 
