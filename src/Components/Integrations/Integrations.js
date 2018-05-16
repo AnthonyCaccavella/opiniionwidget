03f412744e9f0b3a22e93db94a4a1a3bf127a5b1
Anthony Caccavella <anthony@MacBook-Air-2.local>
2018-05-16 09:35:13 -0600

    testing

--- src/Components/Integrations/Integrations.js
+++ src/Components/Integrations/Integrations.js
@@ -98,7 +98,7 @@ export default class Integrations extends Component {
     data: {
       IntegrationPartnerID: 'opiniion',
       ApiKey: 'AAAAB3NzaC1yc2E',
-      AccountID: '800',
+      AccountID: 800,
       PropertyID: '89aa1c41-0212-495b-8e58-1bc60f8de733'
     },
     headers: {
